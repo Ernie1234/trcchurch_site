ea3023470036745d8785db9671140dc43c941da3
unknown <anumahjoshuaeneye@gmail.com>
2024-02-27 19:51:43 +0100

    nav

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -7,61 +7,65 @@ function NavBar() {
   const [isOpen, setIsOpen] = useState(false);
   const pathname = useLocation().pathname;
 
-  const toggleFn = () => {
-    setIsOpen((prev) => !prev);
-  };
-  const closeFn = () => {
-    setIsOpen(false);
-  };
-
   return (
     <>
-      <nav className="w-full sticky top-0 z-50 shadow-md bg-white py-4">
-        <div className="max-w-screen-lg w-full mx-auto my-0 flex justify-between items-center">
+      <nav className="w-full sticky top-0 z-50 shadow-md bg-white">
+        <div className="max-w-screen-xl w-full px-5 md:px-0 h-20 mx-auto my-0 flex justify-between items-center">
           <Link to="/" className="flex items-center self-stretch ">
-            <img alt="Logo" src="/assets/Logo.png" className="w-8" />
+            <img alt="Logo" src="/assets/Logo.png" className="w-12 md:w-16" />
           </Link>
-          <ul className="flex items-center self-stretch ">
-            {navLinks.map((link) => (
-              <Link
-                key={link.title}
-                to={link.url}
-                className={`${
-                  pathname === link.url ? " text-rose-500 " : " text-black"
-                }`}
-              >
-                {link.title}
-              </Link>
-            ))}
-          </ul>
-          <div className="flex gap-3">
-            <div className="block md:hidden text-rose-500">
-              <Menu size={35} onClick={toggleFn} />
+          <div>
+            {" "}
+            <ul className="hidden md:flex items-center self-stretch">
+              {navLinks.map((link) => (
+                <Link
+                  key={link.title}
+                  to={link.url}
+                  className={`${
+                    pathname === link.url
+                      ? "border-b border-rose-500 text-rose-500 font-bold"
+                      : "text-black hover:bg-rose-500 hover:text-white"
+                  } py-0 px-4 font-semibold text-lg capitalize flex items-center h-full transition-all duration-300 cursor-pointer`}
+                >
+                  {link.title}
+                </Link>
+              ))}
+            </ul>
+            <div className="flex gap-3">
+              <div className="block md:hidden text-rose-500">
+                <Menu
+                  size={35}
+                  onClick={() => {
+                    setIsOpen((prev) => !prev);
+                  }}
+                />
+              </div>
             </div>
           </div>
         </div>
       </nav>
       {isOpen && (
-        <div className="bg-linear  h-screen fixed top-0 left-0 right-0 z-[100] opacity-100 w-screen transition-all duration-300">
+        <div className="bg-gradient-to-r from-rose-400 to-red-500 h-screen fixed top-0 left-0 right-0 z-[100] opacity-100 w-screen transition-all duration-300 flex flex-col justify-center items-center">
           <div
-            className="bg-rose-300 text-white p-2 rounded-[50%] flex justify-center items-center absolute left-[50%] top-[10%] transform-[translate(-50%, -50%)] cursor-pointer transition-all duration-300 "
-            onClick={closeFn}
+            className="bg-rose-300 text-white p-2 rounded-[50%] flex justify-center items-center  cursor-pointer transition-all duration-300 "
+            onClick={() => setIsOpen(false)}
           >
             <X size={40} />
           </div>
 
-          <ul className="absolute top-[50%] left-[50%] transform-[translate(-50%, -50%)] list-none text-center w-full">
+          <ul className="flex flex-col justify-center items-center gap-3 list-none text-center w-full mt-20">
             {navLinks.map((link, i) => (
               <Link
                 key={link.title}
                 to={link.url}
-                onClick={closeFn}
-                className={`${pathname === link.url}
-                    ? ' text-black uppercase bg-rose-300 transition-all duration-300'
-                    : ' text-white ' py-1 px-2 inline-block text-5xl font-bold `}
+                onClick={() => setIsOpen(false)}
+                className={`${
+                  pathname === link.url
+                    ? "text-black bg-rose-50 transition-all duration-300"
+                    : "text-white "
+                } py-1 px-2 inline-block text-3xl font-bold capitalize`}
               >
-                <span>{i + 1}</span>
-                {link.title}
+                <span>{i + 1}</span> {link.title}
               </Link>
             ))}
           </ul>
